3ff2f67972c72574617afa19dc9e6a34dbb3df02
chaosbread <chaosbread@phoneticlight.com>
2017-04-14 23:44:51 -0600

    Fix player a controls
    broke it after applying delta move and input map modifications.
    functionality now restored. also naming tanks along A/B convention
    rather than color.

---
+++ Players/player_a.gd
@@ -0,0 +1,16 @@
+extends "res://Players/player.gd"
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	# process movement
+	if(Input.is_action_pressed("player_a_accelerate")):
+		move_forward(delta)
+	if(Input.is_action_pressed("player_a_reverse")):
+		move_backward(delta)
+	if(Input.is_action_pressed("player_a_turn_left")):
+		turn_left(delta)
+	if(Input.is_action_pressed("player_a_turn_right")):
+		turn_right(delta)
+	update()
